02f80239507fb778089f27f5291967c7118e3b6d
thanhlongnet <longprocimex@gmail.com>
2025-07-02 23:13:45 +0700

    Update banana-kaitun.lua

--- banana-kaitun.lua
+++ banana-kaitun.lua
@@ -25,7 +25,7 @@ return {
         ["Enabled"] = true,
         ["Auto Delete Seed Low Price"] = true,
         ["Slot"] = 400,
-        ["Name Seed Delete"] = {"Strawberry", "Blueberry", "Corn", "Cacao", "Tomato", "Orange Tulip", "Carrot", "Beanstalk", "Apple", "Banana", "Succulent", "Rose", "Glowshroom", "Lavender"
+        ["Name Seed Delete"] = {"Strawberry", "Blueberry", "Corn", "Cacao", "Tomato", "Orange Tulip", "Carrot", "Beanstalk", "Apple", "Banana", "Succulent", "Rose", "Glowshroom", "Lavender", "Cactus", "Dragon Fruit"
         }
     },
     ["Dont collect during weather events"] = {
