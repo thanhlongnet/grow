77ffc22318696593ab47c5e930675a29ee6d6f8b
thanhlongnet <longprocimex@gmail.com>
2025-07-02 12:21:18 +0700

    Create banana-kaitun.lua

---
+++ banana-kaitun.lua
@@ -0,0 +1,112 @@
+return {
+    ["Hide UI"] = false,
+    ["Hide Notify"] = true, 
+    ["Buy Egg"] = {
+        ["Select Egg"] = {
+            ["Bee Egg"] = true,
+            ["Bug Egg"] = true,
+            ["Mythical Egg"] = true,
+            ["Legendary Egg"] = true,
+            ["Paradise Egg"] = true,
+            ["Rare Summer Egg"] = true,
+            ["Common Summer Egg"] = true,
+        }
+    },
+    ["Delete Pet"] = {
+        ["Enabled"] = true,
+        ["Pet Dont Delete"] = {"Dragon Fly", "Praying Mantis", "Queen Bee", "Mimic Octopus", "Hyacinth Macaw", "Raccoon", "Blood Owl", "Fennec", "Moon Cat", "Fennec Fox", "Kiwi", "Disco Bee", "Butterfly", "Chicken Zombie"}
+    },
+    
+    ["dont Buy Seed low Price"] = {
+        ["Enabled"] = true, 
+        ["Money"] = 10000000,
+    },
+    ["Auto Delete Seed Planted"] = {
+        ["Enabled"] = false,
+        ["Auto Delete Seed Low Price"] = true,
+        ["Slot"] = 500,
+        ["Name Seed Delete"] = {
+            "",
+        }
+    },
+    ["Dont collect during weather events"] = {
+        ["Enabled"] = false,
+        ["Weather"] = {
+            ["Rain"] = false,
+            ["Frost"] = false,
+            ["Thunderstorm"] = false,
+        }
+    },
+    ["Gear"]  = {
+        ["Buy Gear"] = {
+            ["Enabled"] = true,
+            ["Select Gear"] = {
+                ["Basic Sprinkler"] = true, 
+                ["Advanced Sprinkler"] = true,
+                ["Godly Sprinkler"] = true,
+                ["Master Sprinkler"] = true,
+            }
+        },
+        ["Use Gear"] = {
+            ["Enabled"] = true, 
+            ["Select Gear"] = {
+                ["Basic Sprinkler"] = true, 
+                ["Advanced Sprinkler"] = true,
+                ["Godly Sprinkler"] = true,
+                ["Master Sprinkler"] = true,
+            },
+            ["Stack Gear"] = {
+                ["Enabled"] = false,
+                ["Select Gear"] = { 
+                    ["Basic Sprinkler"] = false, 
+                    ["Advanced Sprinkler"] = false,
+                    ["Godly Sprinkler"] = false,
+                    ["Master Sprinkler"] = false,
+                }
+            }
+        }
+    },
+    ["Webhook"] = {
+        ["Enabled"] = false,
+        ["Url"] = "",
+        ["Webhook Profile"] = true,
+        ["Webhook Collect Egg"] = true,
+        ["Delay"] = 10,
+    },
+    ["Auto Rejoin"] = {
+        ["Enabled"] = true,
+        ["Delay"] = 5
+    },
+    ["FPS Lock"] = {
+        ["Enabled"] = true,
+        ["Value"] = 60
+    },
+    ["Auto Place Pet Egg"] = {
+        ["Enabled"] = true  
+    },
+    ["Summer Event"] = {
+        ["Auto Teleport Summer"] = {
+            ["Enabled"] = false,
+            ["DelayMinute"] = 1
+     },
+        ["SubmitPlant"] = false,
+        ["Delay"] = 2
+    },
+     ["Summer Event Shop"] = {
+        ["Enabled"] = true,
+        ["Times"] = 5
+    },
+    ["Hop Server"] = {
+        ["Enabled"] = false,
+        ["Minutes"] = 15     
+    },
+    ["Auto Craft Setting"] = {
+        ["Enabled"] = true,
+        ["Delay"] = 5,
+        ["Craft Ready"] = true,
+        ["Recipe"] = "Anti Bee Egg"
+    },
+    ["KeyConfig"] = "Bananaconfig-X0CRNAU6",   
+    ["Enable Screen Black"] = true,
+    ["Screen Black FPS Cap"] = 30,
+}
