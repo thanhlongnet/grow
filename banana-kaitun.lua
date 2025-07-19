6e97e70aeb29f4c92bedcc603970087f8667e919
thanhlongnet <longprocimex@gmail.com>
2025-07-19 12:35:24 +0700

    Update banana-kaitun.lua

--- banana-kaitun.lua
+++ banana-kaitun.lua
@@ -1,111 +1,187 @@
-return {
+repeat wait() until game:IsLoaded() and game.Players.LocalPlayer
+
+getgenv().Key = ""
+getgenv().Config = {
     ["Hide UI"] = false,
-    ["Hide Notify"] = true, 
+    ["Hide Notify"] = true,
     ["Buy Egg"] = {
         ["Select Egg"] = {
             ["Bee Egg"] = true,
             ["Bug Egg"] = true,
-            ["Mythical Egg"] = false,
-            ["Legendary Egg"] = false,
+            ["Mythical Egg"] = true,
+            ["Legendary Egg"] = true,
             ["Paradise Egg"] = true,
             ["Rare Summer Egg"] = true,
-            ["Common Summer Egg"] = true,
         }
     },
+
     ["Delete Pet"] = {
         ["Enabled"] = false,
-        ["Pet Dont Delete"] = {"Dragon Fly", "Praying Mantis", "Queen Bee", "Mimic Octopus", "Hyacinth Macaw", "Raccoon", "Blood Owl", "Fennec", "Moon Cat", "Fennec Fox", "Kiwi", "Disco Bee", "Butterfly", "Chicken Zombie", "Starfish", "Toucan", "Moth"}
+        ["Pet Dont Delete"] = {"Queen Bee", "Red Fox", "Dragonfly", "Raccoon", "Disco Bee", "Butterfly",
+            "Mimic Octopus", "Fennec Fox", "Axolotl", "Hyacinth Macaw",
+            "Hamster", "T-Rex", "Wasp", "Moth", "Bear Bee", "Honey Bee", "Petal Bee", "Tarantula Hawk", "Toucan", "Starfish", "Spinosaurus", "Ankylosaurus", "Brontosaurus", "Dilophosaurus"}
     },
-    
+
     ["dont Buy Seed low Price"] = {
-        ["Enabled"] = true, 
-        ["Money"] = 10000000,
+        ["Enabled"] = true,
+        ["Money"] = 1000000
     },
+
     ["Auto Delete Seed Planted"] = {
         ["Enabled"] = true,
         ["Auto Delete Seed Low Price"] = true,
-        ["Slot"] = 400,
-        ["Name Seed Delete"] = {"Strawberry", "Blueberry", "Corn", "Cacao", "Tomato", "Orange Tulip", "Carrot", "Beanstalk", "Apple", "Banana", "Succulent", "Rose", "Glowshroom", "Lavender", "Cactus", "Dragon Fruit", "Mango", "Bell Pepper", "Moon Melon", "Coconut", "Cauliflower", "Watermelon", "Rafflesia"
+        ["Slot"] = 500,
+        ["Name Seed Delete"] = {
+            "Strawberry", "Blueberry", "Corn", "Orange Tulip", "Carrot", "Tomato"
         }
     },
+
     ["Dont collect during weather events"] = {
-        ["Enabled"] = true,
+        ["Enabled"] = false,
         ["Weather"] = {
-            ["Rain"] = true,
-            ["Frost"] = true,
-            ["Thunderstorm"] = true,
+            ["Rain"] = false,
+            ["Frost"] = false,
+            ["Thunderstorm"] = false
         }
     },
-    ["Gear"]  = {
+
+    ["Gear"] = {
         ["Buy Gear"] = {
             ["Enabled"] = true,
             ["Select Gear"] = {
                 ["Basic Sprinkler"] = true, 
                 ["Advanced Sprinkler"] = true,
                 ["Godly Sprinkler"] = true,
-                ["Master Sprinkler"] = true,
+                ["Master Sprinkler"] = true
             }
         },
         ["Use Gear"] = {
-            ["Enabled"] = true, 
+            ["Enabled"] = false,
             ["Select Gear"] = {
                 ["Basic Sprinkler"] = true, 
                 ["Advanced Sprinkler"] = true,
                 ["Godly Sprinkler"] = true,
-                ["Master Sprinkler"] = true,
+                ["Master Sprinkler"] = true
             },
             ["Stack Gear"] = {
                 ["Enabled"] = false,
-                ["Select Gear"] = { 
+                ["Select Gear"] = {
                     ["Basic Sprinkler"] = false, 
                     ["Advanced Sprinkler"] = false,
                     ["Godly Sprinkler"] = false,
-                    ["Master Sprinkler"] = false,
+                    ["Master Sprinkler"] = false
                 }
             }
         }
     },
+
     ["Webhook"] = {
         ["Enabled"] = false,
         ["Url"] = "",
-        ["Webhook Profile"] = true,
-        ["Webhook Collect Egg"] = true,
-        ["Delay"] = 10,
+        ["Webhook Profile"] = false,
+        ["Webhook Collect Egg"] = false,
+        ["Webhook Pet"] = false,
+        ["Webhook Feed Pet"] = false,
+        ["Delay"] = 1
     },
+
     ["Auto Rejoin"] = {
-        ["Enabled"] = true,
+        ["Enabled"] = false,
         ["Delay"] = 5
     },
+
     ["FPS Lock"] = {
         ["Enabled"] = true,
-        ["Value"] = 3
+        ["Value"] = 1
     },
+
     ["Auto Place Pet Egg"] = {
-        ["Enabled"] = true  
+        ["Enabled"] = true
     },
+
     ["Summer Event"] = {
         ["Auto Teleport Summer"] = {
-            ["Enabled"] = true,
+            ["Enabled"] = false,
             ["DelayMinute"] = 1
-     },
-        ["SubmitPlant"] = true,
+        },
+        ["SubmitPlant"] = false,
         ["Delay"] = 2
     },
-     ["Summer Event Shop"] = {
+
+    ["Summer Event Shop"] = {
         ["Enabled"] = true,
         ["Times"] = 5
     },
+
+    ["Buy July 4th Shop"] = {
+        ["Enabled"] = true,
+        ["Items"] = {
+            ["Liberty Lily"] = true,
+            ["Firework Flower"] = true,
+            ["Firework"] = true,
+            ["Bald Eagle"] = true,
+            ["July 4th Crate"] = false
+        }
+    },
+
     ["Hop Server"] = {
         ["Enabled"] = false,
-        ["Minutes"] = 15     
+        ["Minutes"] = 15
     },
+
     ["Auto Craft Setting"] = {
         ["Enabled"] = true,
         ["Delay"] = 5,
         ["Craft Ready"] = true,
         ["Recipe"] = "Anti Bee Egg"
     },
-    ["KeyConfig"] = "Bananaconfig-M93EVX2L",   
+
+    ["Dino Event"] = {
+        ["Trade Egg"] = true,
+        ["Pet Trade"] = {
+            "Queen Bee", "Red Fox", "Dragonfly", "Raccoon", "Disco Bee",
+                "Butterfly", "Mimic Octopus", "Fennec Fox",
+                "Axolotl", "Hyacinth Macaw", "Hamster", "T-Rex", "Wasp", "Moth", "Bear Bee", "Honey Bee", "Petal Bee", "Tarantula Hawk", "Toucan", "Spinosaurus", "Ankylosaurus", "Iguanodon", "Dilophosaurus", "Brontosaurus"
+        },
+        ["Trade All Pet"] = true,
+        ["Claim Reward"] = true,
+        ["Delay"] = 10,
+        ["Craft"] = false
+    },
+
+    ["DinoQuest"] = {
+        ["Quest 1"] = true,
+        ["Quest 2"] = true,
+        ["Quest 3"] = true,
+        ["Claim Dino Quest Done"] = true
+    },
+
+    ["Prehistoric Event"] = {
+        ["Submit All Plant"] = true,
+        ["Submit When Backpack"] = 200,
+        ["Auto Submit Held Pet"] = false,
+        ["Pet Dont Take"] = {
+            "Queen Bee", "Red Fox", "Dragonfly", "Raccoon", "Disco Bee",
+                "Butterfly", "Mimic Octopus", "Fennec Fox",
+                "Axolotl", "Hyacinth Macaw", "Hamster", "T-Rex", "Wasp", "Moth", "Bear Bee", "Honey Bee", "Petal Bee", "Tarantula Hawk", "Toucan", "Spinosaurus", "Ankylosaurus", "Iguanodon", "Dilophosaurus", "Brontosaurus"
+        }
+    },
+
+    ["Feed Pet"] = {
+        ["Enabled"] = false, 
+        ["Feed All"] = false,
+        ["Pet"] = {
+            ""
+        }
+    },
+
+    ["KeyConfig"] = "Bananaconfig-M93EVX2L",
     ["Enable Screen Black"] = true,
-    ["Screen Black FPS Cap"] = 3,
+    ["Screen Black FPS Cap"] = 1
 }
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/main/KaitunGAG.lua"))()
+
+task.delay(6, function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/SkibidiHub111/Premium-/main/BananaConfig"))()
+end)
