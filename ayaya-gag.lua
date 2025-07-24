581ab09d449aeb22c4b811818fba729cf8b04940
thanhlongnet <longprocimex@gmail.com>
2025-07-24 09:20:12 +0700

    Update ayaya-gag.lua

--- ayaya-gag.lua
+++ ayaya-gag.lua
@@ -71,6 +71,17 @@ return {
 	},
 
 	Events = {
+		["Zen Event"] = {
+			["Restocking"] = { -- Minimumthing to restock
+				Minimum_Money = 10_000_000,
+				Minimum_Chi = 200
+			},
+			["Doing"] = {
+				Minimum_Money = 30_000_000, -- minimum money to start play this event
+				First_Upgrade_Tree = 4,
+				Maximum_Chi = 250,
+			}
+		},
 		["Traveling Shop"] = {
 			"Bee Egg",
 		},
@@ -86,8 +97,6 @@ return {
 			"Pet Shard Tranquil",
 			"Koi",
 		},
-		Restocks_limit = 5000000,
-		MinimumChi = 10
 	},
 
 	Gear = {
