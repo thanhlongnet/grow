c6f5ffe7b145ff9001cc16112e697ff6f14c80e0
thanhlongnet <longprocimex@gmail.com>
2025-08-10 09:11:24 +0700

    Update mm.lua

--- mm.lua
+++ mm.lua
@@ -51,7 +51,7 @@ return {
     KEEP_PETS_WEIGHT = { ["Red Giant Ant"] = 5 },
     KEEP_PETS_AGE = { ["Starfish"] = 75 },
 
-    EQUIP_PETS = { ["Gorilla Chef"] = 1, ["Spaghetti Sloth"] = 1, ["Capybara"] = 1, ["Starfish"] = 1, ["French Fry Ferret"] = 1, ["Sunny-Side Chicken"] = 1 },
+    EQUIP_PETS = { ["Gorilla Chef"] = 1, ["Spaghetti Sloth"] = 2, ["Capybara"] = 1, ["Starfish"] = 1, ["French Fry Ferret"] = 1, ["Sunny-Side Chicken"] = 1 },
     USE_PETS_FOR_UPGRADE_SLOT = { "Starfish" },
     REMOVE_PET_MAX_UPGRADE = { "Capybara", "Starfish" },  -- Unequip from garden
 
