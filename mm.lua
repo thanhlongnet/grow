5fd2d50c2d557b215b8ee0af76388d3702e44789
thanhlongnet <longprocimex@gmail.com>
2025-08-16 16:23:42 +0700

    Update mm.lua

--- mm.lua
+++ mm.lua
@@ -54,7 +54,7 @@ return {
                      "Bear Bee", "Moth", "Moon Cat",
                      "Night Owl", "Blood Owl", "Blood Kiwi", "Cooked Owl", "Pterodactyl",
                      "Raiju", "Spaghetti Sloth", "Corrupted Kodama",
-                     "Axolotl", "Echo Frog", "Hamster", "Maneki-neko", "Squirrel", ["Hotdog Daschund"] = 3, ["Sunny-Side Chicken"] =1 , ["Gorilla Chef"] = 4,
+                     "Axolotl", "Echo Frog", "Hamster", "Maneki-neko", ["Squirrel"] = 2, ["Hotdog Daschund"] = 3, ["Sunny-Side Chicken"] =1 , ["Gorilla Chef"] = 4,
                      ["Sushi Bear"] = 1, ["Mochi Mouse"] = 1, ["Koi"] = 1, ["Flamingo"] = 1, ["Bacon Pig"] = 1, ["Junkbot"] = 1,
                      ["Starfish"] = 4, ["Kodama"] = 2, ["Kappa"] = 1, ["Tanchozuru"] = 2,
                      ["Praying Mantis"] = 1, ["Red Giant Ant"] = 1,
