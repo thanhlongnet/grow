3d747df30ce8565ec6c136c313da3393ead60582
thanhlongnet <longprocimex@gmail.com>
2025-08-16 22:32:42 +0700

    Update ayaya-ff.lua

--- ayaya-ff.lua
+++ ayaya-ff.lua
@@ -1,5 +1,5 @@
 return {
-        Beta_Fix_Data_Sync = false,
+        Beta_Fix_Data_Sync = true,
 
         NoDeletePlayer = false,
 
@@ -87,25 +87,23 @@ return {
         },
 
         Events = {
-                ["Cook Event"] = {
-                        Minimum_Money = 10_000_000, -- minimum money to start play this event
-                        Rewards_Item = { -- The top is the most top mean prefered.
-                                "Gorilla Chef",
-                                "Gourmet Egg",
-                                "Culinarian Chest",
-                                "Gourmet Seed Pack",
-                                "Sunny-Side Chicken",
-                                -- u can add it more as u want, if it not in list.
-                        }
-                },
+			["Bean Event"] = {
+				Minimum_Money = 10_000_000, -- minimum money to start play this event
+			},
+			Shop = { -- delete -- to buy
+				"Sprout Seed Pack",
+				"Sprout Egg",
+				-- "Mandrake",
+				"Silver Fertilizer",
+				-- "Canary Melon",
+				-- "Amberheart",
+				"Spriggan",
+		},
                 ["Traveling Shop"] = {
                         "Bee Egg",
                 },
                 Craft = {
                         "Anti Bee Egg",
-                },
-                Shop = {
-                        "Zen Egg",
                 },
                 Start_Do_Honey = 2_000_000 -- start trade fruit for honey at money
         },
