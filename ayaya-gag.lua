8048a7af99c5d8ab1280c7b2ba4e0d4f65fcb48f
thanhlongnet <longprocimex@gmail.com>
2025-08-17 17:53:49 +0700

    Update ayaya-gag.lua

--- ayaya-gag.lua
+++ ayaya-gag.lua
@@ -172,7 +172,7 @@ return {
                      ["Sushi Bear"] = 1, ["Mochi Mouse"] = 1, ["Koi"] = 1, ["Flamingo"] = 1, ["Bacon Pig"] = 1, ["Junkbot"] = 1,
                      ["Starfish"] = 3, ["Kodama"] = 2, ["Kappa"] = 1, ["Tanchozuru"] = 2,
                      ["Giant Ant"] = 1, ["Praying Mantis"] = 1, ["Red Giant Ant"] = 1,
-                     ["Toucan"] = 1, ["Capybara"] = 2, ["Rooster"] = 2, ["Seal"] = 8, ["Chicken"] = 2,
+                     ["Toucan"] = 1, ["Capybara"] = 2, ["Rooster"] = 2, ["Seal"] = 4, ["Chicken"] = 2,
 					 ["Bagel Bunny"] = 2, ["Peacock"] = 2, ["Ostrich"] = 2, ["Orange Tabby"] = 2, ["Sea Turtle"] = 2, ["Scarlet Macaw"] = 2
                 },
                 LockPet_Weight = 7, -- if Weight >= 10 they will locked
