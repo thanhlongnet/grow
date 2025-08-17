a9145fe047fc6a33251c8018e95c43cf4f000aa2
thanhlongnet <longprocimex@gmail.com>
2025-08-17 23:57:06 +0700

    Update mm.lua

--- mm.lua
+++ mm.lua
@@ -4,6 +4,7 @@ return {
     BUY_TRAVELING_MERCHANT = { "Bee Egg", "Loquat", "Feijoa", "Pitcher Plant" },
     MAX_EVENT_RESTOCK_SHECKLES = 500_000_000_001,  -- Restock stop at 48m
     BUY_EVENT_SHOP = { "Sprout Egg", "Spriggan" },
+	PLANT_EVENT_TREES = true,  -- This config will replace most plant related config
 	
     -- General:
     AUTO_UPDATE_RESTART = true,
@@ -12,31 +13,6 @@ return {
     EXTRA_EGG_SLOTS = 5,
     ADD_FRIEND = false,
     OPEN_ALL_SEED_PACK = true,
-
-    MAX_PLANTS = 120,
-    DESTROY_UNTIL_MIN_PLANTS = 80,
-    DELETE_PLANTS_AFTER_MAX = { "Carrot", "Strawberry", "Blueberry", "Tomato", "Apple" },
-    LIMIT_PLANT_SEED = { ["Carrot"] = 3, ["Strawberry"] = 3, ["Blueberry"] = 3, ["Rose"] = 3, ["Orange Tulip"] = 3, ["Dezen"] = 3, ["Artichoke"] = 3,
-       ["Onion"] = 3, ["Tomato"] = 3, ["Daffodil"] = 3, ["Cauliflower"] = 3, ["Raspberry"] = 3, ["Foxglove"] = 3, ["Corn"] = 3,
-       ["Horsetail"] = 3, ["Twisted Tangle"] = 3, ["Jalapeno"] = 3, ["Watermelon"] = 3, ["Pumpkin"] = 3, ["Avocado"] = 3, ["Green Apple"] = 3,
-       ["Apple"] = 3, ["Banana"] = 3, ["Lilac"] = 3, ["Taro Flower"] = 3, ["Bamboo"] = 3, ["Rafflesia"] = 3, ["Lingonberry"] = 3,
-       ["Lucky Bamboo"] = 3, ["Veinpetal"] = 3, ["Crown Melon"] = 3, ["Peach"] = 3, ["Pineapple"] = 3, ["Amber Spine"] = 3, ["Coconut"] = 3,
-       ["Cactus"] = 3, ["Dragon Fruit"] = 3, ["Mango"] = 3, ["Kiwi"] = 3, ["Bell Pepper"] = 3, ["Prickly Pear"] = 3, ["Pink Lily"] = 3,
-       ["Purple Dahlia"] = 3, ["Tall Asparagus"] = 3, ["Sugarglaze"] = 3, ["Grape"] = 3, ["Loquat"] = 3, ["Mushroom"] = 3, ["Pepper"] = 3,
-       ["Cacao"] = 3, ["Feijoa"] = 3, ["Pitcher Plant"] = 3, ["Grand Volcania"] = 3, ["Sunflower"] = 3, ["Grand Tomato"] = 3, ["Taco Fern"] = 3,
-       ["Beanstalk"] = 10, ["Ember Lily"] = 3, ["Sugar Apple"] = 3, ["Burning Bud"] = 3, ["Giant Pinecone"] = 3, ["Elder Strawberry"] = 3, ["Chocolate Carrot"] = 3,
-       ["Red Lollipop"] = 3, ["Nightshade"] = 3, ["Crocus"] = 3, ["Lavender"] = 3, ["Manuka Flower"] = 3, ["Wild Carrot"] = 3, ["Stonebite"] = 3,
-       ["Candy Sunflower"] = 3, ["Peace Lily"] = 3, ["Mint"] = 3, ["Blue Lollipop"] = 3, ["Glowshroom"] = 3, ["Dandelion"] = 3, ["Nectarshade"] = 3,
-       ["Succulent"] = 3, ["Bee Balm"] = 3, ["Pear"] = 3, ["Delphinium"] = 3, ["Liberty Lily"] = 3, ["Paradise Petal"] = 3, ["Cranberry"] = 3, ["Durian"] = 3,
-       ["Papaya"] = 3, ["Moonflower"] = 3, ["Starfruit"] = 3, ["Lumira"] = 3, ["Violet Corn"] = 3, ["Nectar Thorn"] = 3, ["Cantaloupe"] = 3, ["Aloe Vera"] = 3,
-       ["Firework Flower"] = 3, ["White Mulberry"] = 3, ["Dragon Sapling"] = 3, ["Horned Dinoshroom"] = 3, ["Boneboo"] = 3, ["Fruitball"] = 3, ["Enkaku"] = 3,
-       ["Sakura Bush"] = 3, ["Easter Egg"] = 3, ["Eggplant"] = 3, ["Passionfruit"] = 3, ["Lemon"] = 3, ["Moonglow"] = 3, ["Moon Melon"] = 3, ["Blood Banana"] = 3,
-       ["Celestiberry"] = 3, ["Guanabana"] = 3, ["Nectarine"] = 3, ["Honeysuckle"] = 3, ["Suncoil"] = 3, ["Bendboo"] = 3, ["Cocovine"] = 3, ["Parasol Flower"] = 3,
-       ["Lily Of The Valley"] = 3, ["Firefly Fern"] = 3, ["Moon Mango"] = 3, ["Candy Blossom"] = 3, ["Cherry Blossom"] = 3, ["Lotus"] = 3, ["Venus Fly Trap"] = 3,
-       ["Rosy Delight"] = 3, ["Traveler's Fruit"] = 3, ["Fossilight"] = 3, ["Tranquil Bloom"] = 3, ["Elephant Ears"] = 3, ["Bone Blossom"] = 3, ["Pink Tulip"] = 3,
-       ["Noble Flower"] = 3, ["Purple Cabbage"] = 3, ["Sinisterdrip"] = 3, ["Mega Mushroom"] = 3, ["Ice Cream Bean"] = 3, ["Lime"] = 3, ["Crimson Vine"] = 3, ["Zenflare"] = 3,
-       ["Serenity"] = 3, ["Zen Rocks"] = 3, ["Hinomai"] = 3, ["Monobloom"] = 3, ["Badlands Pepper"] =2, ["Bitter Melon"] = 3, ["King Cabbage"] = 3, ["Flare Daisy"] = 3, ["Mangosteen"] = 3,
-},
     
     BUY_EGGS = { "Bug Egg", "Bee Egg", "Paradise Egg", "Mythical Egg", "Rare Summer Egg", "Common Summer Egg", "Rare Egg", "Uncommon Egg" },
     PLANT_EGGS = { "Sprout Egg", "Gourmet Egg", "Corrupted Zen Egg", "Zen Egg", "Dinosaur Egg", "Primal Egg", "Anti Bee Egg", "Night Egg", "Bug Egg", "Paradise Egg"
@@ -44,8 +20,8 @@ return {
 	},
     
     BUY_SEED_SHOP = { "Romanesco", "Elder Strawberry", "Giant Pinecone", "Burning Bud", "Sugar Apple", "Ember Lily", "Beanstalk", "Cacao", "Pepper", "Mushroom", "Grape", "Mango", "Dragon Fruit", "Cactus", ["Coconut"] = 50, ["Bamboo"] = 50, ["Apple"] = 50, ["Pumpkin"] = 50, ["Watermelon"] = 50, ["Daffodil"] = 50, ["Tomato"] = 50, ["Orange Tulip"] = 50, ["Blueberry"] = 50, ["Strawberry"] = 50, ["Carrot"] = 50 },
-    KEEP_SEEDS = { "Bone Blossom" },
-    KEEP_SEEDS_AFTER_MAX_PLANTS = { "Carrot" },
+    KEEP_SEEDS = { "Carrot" },
+    KEEP_SEEDS_AFTER_MAX_PLANTS = {}
     
     FAVOURITE_FRUIT_MUTATIONS = {},  -- Stop Autosell
     SKIP_HARVEST_MUTATIONS = {},  -- Stop Harvest
