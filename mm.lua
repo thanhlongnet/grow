1c655abd4b408fdc1aadc5bb4d92ed390e8c5b66
thanhlongnet <longprocimex@gmail.com>
2025-09-05 23:00:24 +0700

    Update mm.lua

--- mm.lua
+++ mm.lua
@@ -41,7 +41,7 @@ return {
     KEEP_PETS_WEIGHT = 8,
     KEEP_PETS_AGE = 90,
 
-    EQUIP_PETS = { ["Wasp"] = 3, ["Glimmering Sprite"] = 1, ["Tarantula Hawk"] = 3, ["Starfish"] = 1, ["Rooster"] = 2, ["Sunny-Side Chicken"] = 1},
+    EQUIP_PETS = { ["Wasp"] = 2, ["Glimmering Sprite"] = 1, ["Tarantula Hawk"] = 2, ["Starfish"] = 1, ["Rooster"] = 2, ["Sunny-Side Chicken"] = 1, ["Griffin"] =1},
     USE_PETS_FOR_UPGRADE_SLOT = { "Starfish" },
     REMOVE_PET_MAX_UPGRADE = { "Capybara", "Starfish" },  -- Unequip from garden
 
