da0596728938ebf726962007ecbdf789b4c66b3d
thanhlongnet <longprocimex@gmail.com>
2025-09-05 22:58:22 +0700

    Update mm.lua

--- mm.lua
+++ mm.lua
@@ -15,7 +15,7 @@ return {
     OPEN_ALL_SEED_PACK = true,
     
     BUY_EGGS = { "Bug Egg", "Bee Egg", "Paradise Egg", "Mythical Egg", "Rare Summer Egg", "Common Summer Egg", "Legendary Egg", "Mythical Egg", "Rare Egg", "Uncommon Egg"},
-    PLANT_EGGS = { "Anti Bee Egg", "Enchanted Egg", "Sprout Egg", "Gourmet Egg", "Legendary Egg", "Corrupted Zen Egg", "Zen Egg", "Dinosaur Egg", "Primal Egg", "Night Egg", "Bug Egg", "Paradise Egg", "Common Summer Egg",
+    PLANT_EGGS = { "Anti Bee Egg", "Enchanted Egg", "Sprout Egg", "Gourmet Egg", "Rare Egg", "Legendary Egg", "Corrupted Zen Egg", "Zen Egg", "Dinosaur Egg", "Primal Egg", "Night Egg", "Bug Egg", "Paradise Egg", "Common Summer Egg",
 	},
     
     BUY_SEED_SHOP = { "Romanesco", "Elder Strawberry", "Giant Pinecone", "Burning Bud", "Sugar Apple", "Ember Lily", "Beanstalk", "Cacao", "Pepper", "Mushroom", "Grape", "Mango", "Dragon Fruit", "Cactus", ["Corn"] = 50, ["Coconut"] = 50, ["Bamboo"] = 50, ["Apple"] = 50, ["Pumpkin"] = 50, ["Watermelon"] = 50, ["Daffodil"] = 50, ["Tomato"] = 50, ["Orange Tulip"] = 50, ["Blueberry"] = 50, ["Strawberry"] = 50, ["Carrot"] = 50 },
@@ -41,7 +41,7 @@ return {
     KEEP_PETS_WEIGHT = 8,
     KEEP_PETS_AGE = 90,
 
-    EQUIP_PETS = { ["Wasp"] = 3, ["Glimmering Sprite"] = 1, ["Tarantula Hawk"] = 3, ["Starfish"] = 1, },
+    EQUIP_PETS = { ["Wasp"] = 3, ["Glimmering Sprite"] = 1, ["Tarantula Hawk"] = 3, ["Starfish"] = 1, ["Rooster"] = 2, ["Sunny-Side Chicken"] = 1,
     USE_PETS_FOR_UPGRADE_SLOT = { "Starfish" },
     REMOVE_PET_MAX_UPGRADE = { "Capybara", "Starfish" },  -- Unequip from garden
 
