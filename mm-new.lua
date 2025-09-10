8a763f6077e0d2e2b6ca54a1623c21ea6ebbd42b
thanhlongnet <longprocimex@gmail.com>
2025-09-11 01:06:46 +0700

    Update mm-new.lua

--- mm-new.lua
+++ mm-new.lua
@@ -44,10 +44,10 @@ return {
                      ["Starfish"] = 5, ["Tanchozuru"] = 3,
                      ["Praying Mantis"] = 10,
                      ["Rooster"] = 8, ["Seal"] = 3, ["Chicken"] = 2,
-					 ["Peacock"] = 3, ["Ostrich"] = 4, ["Turtle"] = 3, ["Sea Turtle"] = 3, ["Scarlet Macaw"] = 3, ["Sunny-Side Chicken"] = 3, ["Golden Lab"] = 15},
+					 ["Peacock"] = 3, ["Ostrich"] = 4, ["Turtle"] = 3, ["Sea Turtle"] = 3, ["Scarlet Macaw"] = 3, ["Sunny-Side Chicken"] = 3, ["Golden Lab"] = 15, ["Chicken"] = 3},
     KEEP_PETS_WEIGHT = 6,
     KEEP_PETS_AGE = 90,
-	EQUIP_PETS = { ["Wasp"] = 2, ["Tarantula Hawk"] = 2, ["Golden Lab"] = 6, ["Starfish"] = 2, ["Luminous Sprite"] = 1, ["Capybara"] = 1, ["Rooster"] = 4},
+	EQUIP_PETS = { ["Wasp"] = 2, ["Tarantula Hawk"] = 2, ["Golden Lab"] = 6, ["Chicken"] = 3, ["Starfish"] = 2, ["Luminous Sprite"] = 1, ["Capybara"] = 1, ["Rooster"] = 4},
     --EQUIP_PETS = { ["Rooster"] = 8, ["Starfish"] = 2, ["Tarantula Hawk"] = 1, ["Wasp"] = 1, ["Luminous Sprite"] = 8 },
     USE_PETS_FOR_UPGRADE_SLOT = { "Golden Lab" },
     REMOVE_PET_MAX_UPGRADE = { "Golden Lab" },  -- Unequip from garden
