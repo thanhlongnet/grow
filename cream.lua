65821cbe0cb527d155327fb92d3f04af2309fbdc
thanhlongnet <longprocimex@gmail.com>
2025-09-13 22:27:30 +0700

    Create cream.lua

---
+++ cream.lua
@@ -0,0 +1,51 @@
+getgenv().Config = {
+    AUTO_RESTART_WHEN_UPDATE = true,
+    AUTO_ACHIEVEMENTS = false,
+    CRAFT_ITEMS = {},
+
+    AUTO_GAREN_FOR_EVENT = true,
+
+    BUY_SEED_SHOP = {"All"},
+    BUY_GEAR_SHOP = {"Trading Ticket", "Trowel", "Recall Wrench", "Basic Sprinkler", "Advanced Sprinkler", "Godly Sprinkler", "Master Sprinkler", "Grandmaster Sprinkler", "Medium Toy", "Medium Treat", "Levelup Lollipop"},
+    BUY_EGG_SHOP = {"All"},
+    BUY_EVENT_SHOP = {"All"},
+    BUY_MERCHANT_SHOP = {"All"},
+    BUY_HONEY_MERCHANT = false,
+
+    PLACE_SPRINKLERS = {"Basic Sprinkler", "Advanced Sprinkler", "Godly Sprinkler", "Master Sprinkler", "Grandmaster Sprinkler"},
+    PLACE_EGGS = {"Fall Egg"},
+    EQUIP_COSMETICS = {["Cooking Kit"] = 8},
+    EQUIP_PETS = {["Rooster"] = 8},
+
+    LOCK_PET_NAME = {"Barn Owl", "Swan", "Marmot", "Space Squirrel", "Sugar Glider", "Phoenix", "Drake", "Luminous Sprite", "Cockatrice", "Glimmering Sprite", "Shroomie", "Gnome", "Griffin", "Apple Gazelle", "Green Bean", "Lemon Lion", "Golden Goose", "Kitsune", "Corrupted Kitsune", "French Fry Ferret", "Lobster Thermidor",
+                      "T-Rex", "Spinosaurus", "Fennec Fox", "Mimic Octopus", "Disco Bee",
+                      "Butterfly", "Raccoon", "Queen Bee", "Dragonfly", "Red Fox",
+                      "Ankylosaurus", "Dilophosaurus", "Brontosaurus", "Hyacinth Macaw",
+                      "Moth", "Moon Cat",
+                      "Night Owl", "Blood Owl", "Blood Kiwi", "Cooked Owl",
+                      "Raiju", "Chicken Zombie", "Wasp", "Peach Wasp", "Tarantula Hawk",
+                      "Axolotl", "Echo Frog", "Hamster", "Maneki-neko",
+                      "Koi",
+                      "Starfish", "Tanchozuru",
+                      "Praying Mantis", -- ĐÃ SỬA LỖI TẠI ĐÂY
+                      "Rooster", "Seal", "Chicken",
+                      "Peacock", "Ostrich", "Turtle", "Sea Turtle", "Scarlet Macaw", "Sunny-Side Chicken", "Capybara"},
+    LOCK_PET_RARITY = {"Divine", "Prismatic"},
+    LOCK_PET_COUNT = {["Sunny-Side Chicken"] = 8, ["Rooster"] = 8, ["Glimmering Sprite"] = 5, ["Shroomie"] = 5,["Starfish"] = 5, ["Tanchozuru"] = 3,["Praying Mantis"] = 10, ["Seal"] = 3, ["Chicken"] = 2,["Peacock"] = 3, ["Ostrich"] = 4, ["Turtle"] = 3, ["Sea Turtle"] = 3, ["Scarlet Macaw"] = 3},
+    LOCK_PET_WEIGHT = 5,
+    LOCK_PET_AGE = 75,
+    EXTRA_PET_SLOT = 5,
+    EXTRA_EGG_SLOT = 5,
+
+    LIST_USERNAME = {},
+    GIFT_PET_NAME = {"Mimic Octopus", "Butterfly", "Dilophosaurus"},
+    GIFT_PET_RARITY = {"Divine", "Prismatic"},
+    ACCEPT_GIFT = true,
+    SHUTDOWN_AFTER_GIFT_PET = true,
+
+    NOTIFY_PET_NAME = {"Mimic Octopus", "Butterfly", "Dilophosaurus"},
+    NOTIFY_PET_RARITY = {"Divine", "Prismatic"},
+    NOTIFY_PET_WEIGHT = 5,
+    WEBHOOK_URL = "",
+    WEBHOOK_NOTE = "",
+}
