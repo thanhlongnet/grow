3adaf6084d9954822dc6c7753e20e5f78e429a78
thanhlongnet <longprocimex@gmail.com>
2025-09-20 18:28:28 +0700

    Delete ya.lua

--- ya.lua
+++
@@ -1,8 +0,0 @@
-local configUrl = "https://raw.githubusercontent.com/thanhlongnet/grow/refs/heads/main/ayaya-gag.lua"
-
-License = "1C2zisUfOtxKcjkz7Nv5mdpkbYZoqU46"
-
-pcall(function()
-    getgenv().ConfigsKaitun = loadstring(game:HttpGet(configUrl))()
-end)
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Real-Aya/Loader/main/Init.lua'))()
