c1331325edcecf0377fd7888c20b61c52550d858
thanhlongnet <longprocimex@gmail.com>
2025-09-20 17:22:55 +0700

    Update mm-long.lua

--- mm-long.lua
+++ mm-long.lua
@@ -24,7 +24,7 @@ return {
     BUY_EGGS = { "Bug Egg", "Bee Egg", "Paradise Egg", "Mythical Egg", "Rare Summer Egg", "Common Summer Egg",
         -- "Legendary Egg",
         "Rare Egg"},
-    PLANT_EGGS = { "Rare Egg",
+    PLANT_EGGS = { 
          -- "Legendary Egg", "Anti Bee Egg", "Fall Egg", "Common Summer Egg", "Bug Egg", "Enchanted Egg", "Sprout Egg", "Gourmet Egg", "Corrupted Zen Egg", "Zen Egg", "Dinosaur Egg", "Primal Egg", "Night Egg", "Paradise Egg",
     },
     
