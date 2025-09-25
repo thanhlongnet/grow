3fc722d129d6848048e43fd4f261b15ec6fc2835
thanhlongnet <longprocimex@gmail.com>
2025-09-26 01:24:39 +0700

    Create pvb.lua

---
+++ pvb.lua
@@ -0,0 +1,26 @@
+local publicConfig = {
+    AUTO_UPDATE_RESTART = true,
+    MAX_FPS = 3,
+    LOW_CPU = true,
+    MAX_REBIRTH = 2,
+    BRAINROT_RARITY_STOP_REBIRTH = {"Secret", "Limited"},
+
+    BUY_SEED_SHOP = {["Cactus"] = 5, ["Strawberry"] = 5, ["Pumpkin"] = 5, ["Sunflower"] = 5, ["Dragon Fruit"] = 5, ["Eggplant"] = 5, ["Watermelon"] = 5, "Cocotank", "Carnivorous Plant", "Mr Carrot", "Tomatrio"},
+    BUY_GEAR_SHOP = {},
+    KEEP_SEED = {},
+    KEEP_PLANT = {},
+    KEEP_PLANT_RARITY = {"Secret", "Limited"},
+    KEEP_BRAINROT = {},
+    KEEP_BRAINROT_WEIGHT = math.huge,
+    KEEP_BRAINROT_MONEY_PER_SECOND = math.huge,
+    KEEP_BRAINROT_RARITY = {"Secret", "Limited"},
+
+    SELL_BRAINROT_DELAY = 20,
+    SELL_PLANT_DELAY = 20,
+}
+
+if getgenv().pvbConfig then
+    for key, value in pairs(publicConfig) do
+        getgenv().pvbConfig[key] = value
+    end
+end
