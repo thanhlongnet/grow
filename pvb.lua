c9b10ad44e722a4e756e9f101d01e63490199cdc
thanhlongnet <longprocimex@gmail.com>
2025-09-26 21:50:35 +0700

    Update pvb.lua

--- pvb.lua
+++ pvb.lua
@@ -1,7 +1,7 @@
 local publicConfig = {
     AUTO_UPDATE_RESTART = true,
     MAX_FPS = 3,
-    LOW_CPU = true,
+    LOW_CPU = false,
     MAX_REBIRTH = 2,
     BRAINROT_RARITY_STOP_REBIRTH = {"Secret", "Limited"},
 
