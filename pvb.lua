410df25ea805c745e6d3865f5419ecd3f15a69d6
thanhlongnet <longprocimex@gmail.com>
2025-09-30 00:41:53 +0700

    Update pvb.lua

--- pvb.lua
+++ pvb.lua
@@ -2,23 +2,22 @@ local publicConfig = {
     AUTO_UPDATE_RESTART = true,
     MAX_FPS = 3,
     LOW_CPU = true,
-    MAX_REBIRTH = 2,
-    BRAINROT_RARITY_STOP_REBIRTH = {"Secret", "Limited"},
+    MAX_REBIRTH = 99,
+    FROST_GRENADE_TARGET_MAX_HP = 100000,  -- Use frost grenade 100k+ hp brainrot
+
     OPEN_LUCKY_EGG = {"Godly Lucky Egg", "Secret Lucky Egg", "Meme Lucky Egg"},
     FUSE_PLANT = {},
 
-    BUY_SEED_SHOP = {["Cactus"] = 5, ["Strawberry"] = 5, ["Pumpkin"] = 5, ["Sunflower"] = 5, ["Dragon Fruit"] = 5, ["Eggplant"] = 5, ["Watermelon"] = 5, "Cocotank", "Carnivorous Plant", "Mr Carrot", "Tomatrio"},
-    BUY_GEAR_SHOP = {},
+    BUY_SEED_SHOP = {["Cactus"] = 5, ["Strawberry"] = 5, ["Pumpkin"] = 5, ["Sunflower"] = 5, ["Dragon Fruit"] = 5, ["Eggplant"] = 5, ["Watermelon"] = 5, "Cocotank", "Carnivorous Plant", "Mr Carrot", "Tomatrio", "Shroombino"},
+    BUY_GEAR_SHOP = {"Frost Grenade", "Frost Blower"},
     KEEP_SEED = {},
-    KEEP_PLANT = {"Mr Carrot", "Tomatrio"},
     KEEP_PLANT_RARITY = {"Secret", "Limited"},
     KEEP_BRAINROT = {"Tralalelo Tralala", "Garamararam", "Los Tralaleritos", "Vacca Saturno Saturnita"},
-    KEEP_BRAINROT_WEIGHT = math.huge,
-    KEEP_BRAINROT_MONEY_PER_SECOND = math.huge,
+    KEEP_BRAINROT_MONEY_PER_SECOND = 2000,  -- Number
     KEEP_BRAINROT_RARITY = {"Secret", "Limited"},
 
-    SELL_BRAINROT_DELAY = 20,
-    SELL_PLANT_DELAY = 20,
+    SELL_BRAINROT_DELAY = 30,
+    SELL_PLANT_DELAY = 30,
 }
 
 if getgenv().pvbConfig then
