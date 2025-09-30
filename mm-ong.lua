d10217a86022061c040b080a5fa5736d7df521f3
thanhlongnet <longprocimex@gmail.com>
2025-09-30 14:19:40 +0700

    Update mm-ong.lua

--- mm-ong.lua
+++ mm-ong.lua
@@ -35,7 +35,7 @@ return {
     FAVOURITE_FRUIT_MUTATIONS = {},  -- Stop Autosell
     SKIP_HARVEST_MUTATIONS = {},  -- Stop Harvest
 
-    KEEP_PETS = { "Firefly", "Silver Dragonfly", ["Iguana"] = 5,["Hummingbird"] = 5, ["Tree Frog"] =5, "Tiger", ["Chimpanzee"] = 5, ["Blue Jay"] = 5, "Red Panda", "Barn Owl", "Swan", "Space Squirrel", "Phoenix", ["Wisp"] = 5,
+    KEEP_PETS = { "Firefly", "Silver Dragonfly", ["Hummingbird"] = 5, "Tiger", ["Chimpanzee"] = 5, ["Blue Jay"] = 5, "Red Panda", "Barn Owl", "Swan", "Space Squirrel", "Phoenix", ["Wisp"] = 5,
         "Luminous Sprite", "Cockatrice", "Gnome", "Griffin", "Apple Gazelle",
         "Green Bean", "Golden Goose", "Kitsune", "Corrupted Kitsune", "French Fry Ferret", "Lobster Thermidor",
         "T-Rex", "Spinosaurus", "Fennec Fox", "Mimic Octopus", "Disco Bee",
