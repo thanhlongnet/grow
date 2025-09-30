800d45c9bf4957ab8177dee61d579d60f173d2bd
thanhlongnet <longprocimex@gmail.com>
2025-09-30 19:13:49 +0700

    Update pvb.lua

--- pvb.lua
+++ pvb.lua
@@ -6,7 +6,7 @@ local publicConfig = {
     FROST_GRENADE_TARGET_MAX_HP = 100000,  -- Use frost grenade 100k+ hp brainrot
 
     OPEN_LUCKY_EGG = {"Godly Lucky Egg", "Secret Lucky Egg", "Meme Lucky Egg"},
-    FUSE_PLANT = {},
+    FUSE_PLANT = {"Mr Carrot"},
 
     BUY_SEED_SHOP = {["Cactus"] = 5, ["Strawberry"] = 5, ["Pumpkin"] = 5, ["Sunflower"] = 5, ["Dragon Fruit"] = 5, ["Eggplant"] = 5, ["Watermelon"] = 5, "Cocotank", "Carnivorous Plant", "Mr Carrot", "Tomatrio", "Shroombino"},
     BUY_GEAR_SHOP = {"Frost Grenade", "Frost Blower"},
