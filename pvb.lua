dd6f7605569c518379f42faa9ad58e575234343d
thanhlongnet <longprocimex@gmail.com>
2025-10-05 00:45:23 +0700

    Update pvb.lua

--- pvb.lua
+++ pvb.lua
@@ -1,20 +1,21 @@
 local publicConfig = {
     AUTO_UPDATE_RESTART = true,
-    MAX_FPS = 3,
+    MAX_FPS = 3, -- This will override setfpscap()
     LOW_CPU = true,
-    MAX_REBIRTH = 99,
-    FORCE_REBIRTH_IGNORE_KEEP_BRAINROT = true,
-    FROST_GRENADE_TARGET_MAX_HP = 100000,  -- Use frost grenade 100k+ hp brainrot
+    MAX_REBIRTH = 99, -- Stop rebirth at set amount
+    FORCE_REBIRTH_IGNORE_KEEP_BRAINROT = true, -- Ignore KEEP_BRAINROT related config until max rebirth
+    FROST_GRENADE_TARGET_MAX_HP = 100000, -- Use frost grenade 100k+ hp brainrot
 
     OPEN_LUCKY_EGG = {"Godly Lucky Egg", "Secret Lucky Egg", "Meme Lucky Egg"},
-    FUSE_PLANT = {"Mr Carrot"},
+    FUSE_PLANT = {"Mr Carrot"}, -- Auto keep & fuse required plant + brainrot
 
-    BUY_SEED_SHOP = {["Cactus"] = 5, ["Strawberry"] = 5, ["Pumpkin"] = 5, ["Sunflower"] = 5, ["Dragon Fruit"] = 5, ["Eggplant"] = 5, ["Watermelon"] = 5, "Cocotank", "Carnivorous Plant", "Mr Carrot", "Tomatrio", "Shroombino"},
+    BUY_SEED_SHOP = {["Cactus"] = 5, ["Strawberry"] = 5, ["Pumpkin"] = 5, ["Sunflower"] = 5, ["Dragon Fruit"] = 5, ["Eggplant"] = 5, ["Watermelon"] = 5, "Cocotank", "Carnivorous Plant", "Mr Carrot", "Tomatrio", "Shroombino", "Mango"},
     BUY_GEAR_SHOP = {"Frost Grenade", "Frost Blower"},
+
     KEEP_SEED = {},
     KEEP_PLANT_RARITY = {"Secret", "Limited"},
-    KEEP_BRAINROT_MONEY_PER_SECOND = 2000,  -- Number
-    KEEP_BRAINROT_RARITY = {"Secret", "Limited"},
+    KEEP_BRAINROT_MONEY_PER_SECOND = 20000, -- Number
+    KEEP_BRAINROT_RARITY = {},
 
     SELL_BRAINROT_DELAY = 30,
     SELL_PLANT_DELAY = 30,
