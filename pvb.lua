f08d63a2e7c098c81400c51a09dcddb94452e544
thanhlongnet <longprocimex@gmail.com>
2025-10-11 22:14:52 +0700

    Update pvb.lua

--- pvb.lua
+++ pvb.lua
@@ -1,29 +1,36 @@
-local publicConfig = {
+getgenv().pvbConfig = {
     AUTO_UPDATE_RESTART = true,
     MAX_FPS = 3, -- This will override setfpscap()
     LOW_CPU = true,
     MAX_REBIRTH = 99, -- Stop rebirth at set amount
     FORCE_REBIRTH_IGNORE_KEEP_BRAINROT = true, -- Ignore KEEP_BRAINROT related config until max rebirth
     FROST_GRENADE_TARGET_MAX_HP = 100000, -- Use frost grenade 100k+ hp brainrot
-
+    CONSUME_POTION_EVENT = {"Frozen", "Golden", "Rainbow", "Galactic", "Underworld", "UpsideDown", "Volcano"},
+    PLANT_SECRET_LIMITED_SEED_EVENT = {"Frozen", "Golden", "Rainbow", "Galactic", "Underworld", "UpsideDown", "Volcano"},
     OPEN_LUCKY_EGG = {"Godly Lucky Egg", "Secret Lucky Egg", "Meme Lucky Egg"},
+    OPEN_CARD_PACK = {"Base", "Shiny Base"},
     FUSE_PLANT = {"Mr Carrot", "Watermelon", "Eggplant", "Dragon Fruit", "Sunflower", "Pumpkin"}, -- Auto keep & fuse required plant + brainrot
 
-    BUY_SEED_SHOP = {["Cactus"] = 5, ["Strawberry"] = 5, ["Pumpkin"] = 5, ["Sunflower"] = 5, ["Dragon Fruit"] = 5, ["Eggplant"] = 5, ["Watermelon"] = 5, "Cocotank", "Carnivorous Plant", "Mr Carrot", "Tomatrio", "Shroombino", "Mango", "King Limone"},
-    BUY_GEAR_SHOP = {"Frost Grenade", "Frost Blower"},
+    BUY_SEED_SHOP = {["Cactus"] = 5, ["Strawberry"] = 5, ["Pumpkin"] = 5, ["Sunflower"] = 5, ["Dragon Fruit"] = 5, ["Eggplant"] = 5, ["Watermelon"] = 5, ["Cocotank"] = 5, "Carnivorous Plant", "Mr Carrot", "Tomatrio", "Shroombino", "Mango", "King Limone"},
+    BUY_GEAR_SHOP = {["Frost Grenade"] = 100, ["Frost Blower"] = 100},
 
     KEEP_SEED = {},
     KEEP_PLANT_RARITY = {"Secret", "Limited"},
-    KEEP_BRAINROT = {"Bredda Ratto", "Squalo Cavallo ", "Dragon Cannelloni", "Gattolini Owlini", "Pot Hotspot", "Crazylone Pizaione", "La Tomatoro", "Los Tralaleritos", "Los Mr Carrotitos"},
+    KEEP_BRAINROT = {"Crazylone Pizaione"},
     KEEP_BRAINROT_MONEY_PER_SECOND = 20000, -- Number
     KEEP_BRAINROT_RARITY = {},
 
     SELL_BRAINROT_DELAY = 30,
     SELL_PLANT_DELAY = 30,
-}
 
-if getgenv().pvbConfig then
-    for key, value in pairs(publicConfig) do
-        getgenv().pvbConfig[key] = value
-    end
-end
+    -- Webhook
+    BRAINROT_WEBHOOK_URL = "",
+    DISCORD_ID = "",
+    NOTIFY_RARITY = {},
+    NOTIFY_MONEY_PER_SECOND = 10000,
+    WEBHOOK_NOTE = "",
+    SHOW_PUBLIC_DISCORD_ID = false,
+    SHOW_WEBHOOK_USERNAME = false,
+    SHOW_WEBHOOK_JOBID = false,
+
+}
