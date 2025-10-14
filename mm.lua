f2c9cfb1d39b02565fae33af8980896e2b2efabb
thanhlongnet <longprocimex@gmail.com>
2025-10-14 08:49:21 +0700

    Update mm.lua

--- mm.lua
+++ mm.lua
@@ -23,7 +23,7 @@ return {
     BUY_COSMETIC_SHOP = { ["Cooking Kit"] = 8 },
 
     BUY_EGGS = { "Spooky Egg", "Jungle Egg", "Bug Egg", "Bee Egg", "Paradise Egg", "Mythical Egg", "Rare Summer Egg", "Common Summer Egg", "Rare Egg", "Uncommon Egg"},
-    PLANT_EGGS = { "Spooky Egg", },
+    PLANT_EGGS = { "Spooky Egg", "Rare Egg",},
     
     BUY_SEED_SHOP = { "Romanesco", "Elder Strawberry", "Giant Pinecone", "Burning Bud", "Sugar Apple", "Ember Lily", "Beanstalk", "Cacao", "Pepper", "Mushroom", "Grape", "Mango", "Dragon Fruit", "Cactus", ["Corn"] = 50, ["Coconut"] = 50, ["Bamboo"] = 50, ["Apple"] = 50, ["Pumpkin"] = 50, ["Watermelon"] = 50, ["Daffodil"] = 50, ["Tomato"] = 50, ["Orange Tulip"] = 50, ["Blueberry"] = 50, ["Strawberry"] = 50, ["Carrot"] = 50 },
 
